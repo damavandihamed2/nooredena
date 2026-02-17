ecf1ca46393dcf58309bbb72176feb1089a2bde8
Hamed Damavandi <damavandi.hamed6@yahoo.com>
2026-02-17 17:08:40 +0330

    Update sigma_crawler.py

--- sigma/sigma_crawler.py
+++ sigma/sigma_crawler.py
@@ -1,7 +1,9 @@
 import pandas as pd
-import sigma, warnings, jdatetime
+import warnings, jdatetime
 
+from sigma import sigma
 from utils.database import make_connection, insert_to_database
+
 
 warnings.filterwarnings("ignore")
 powerbi_database = make_connection()
@@ -57,90 +59,102 @@ cols_mapper_dividend = {
 agent = sigma.Agent(username=authenticator[0]["username"], password=authenticator[0]["password"])
 agent.login()
 
+
 ##############################
 ##### Updating BuySell Data
-portfoId = 1
-query_last_buysell = f"select max(date) date FROM [nooredenadb].[sigma].[buysell] WHERE portfolio_id={portfoId}"
-last_buysell = pd.read_sql(query_last_buysell, powerbi_database)["date"].iloc[0]
-start_buysell = (jdatetime.datetime.strptime(last_buysell, format="%Y/%m/%d") + one_day).strftime("%Y/%m/%d")
+for pid in [1, 25]:
+    portfoId = pid
+    last_buysell = pd.read_sql(f"select max(date) date FROM [nooredenadb].[sigma].[buysell] "
+                               f"WHERE portfolio_id={portfoId}", powerbi_database)["date"].iloc[0]
+    start_buysell = (jdatetime.datetime.strptime(last_buysell, format="%Y/%m/%d") + one_day).strftime("%Y/%m/%d")
 
-buysell_data = agent.get_buysell(start_date=start_buysell, end_date=today_, portfolio_id=portfoId)
-if len(buysell_data) > 0:
-    buysell_df = pd.DataFrame(buysell_data)
-    buysell_df = buysell_df[list(cols_mapper_buysell.keys())]
-    buysell_df.rename(mapper=cols_mapper_buysell, axis=1, inplace=True)
-    buysell_df = buysell_df.astype({
-        "broker_cost": "int64", "organization_cost": "int64", "tax_cost": "int64", "broker_discount": "int64",
-        "organization_discount": "int64", "tax_discount": "int64", "total_discount": "int64", "net_value": "int64",
-        "total_cost": "int64", "net_profit": "int64", "gross_profit": "int64"})
-    buysell_df["portfolio_id"] = portfoId
-    insert_to_database(dataframe=buysell_df, database_table="[nooredenadb].[sigma].[buysell]")
+    buysell_data = agent.get_buysell(start_date=start_buysell, end_date=today_, portfolio_id=portfoId)
+    if len(buysell_data) > 0:
+        buysell_df = pd.DataFrame(buysell_data)
+        buysell_df = buysell_df[list(cols_mapper_buysell.keys())]
+        buysell_df.rename(mapper=cols_mapper_buysell, axis=1, inplace=True)
+        buysell_df = buysell_df.astype({
+            "broker_cost": "int64", "organization_cost": "int64", "tax_cost": "int64", "broker_discount": "int64",
+            "organization_discount": "int64", "tax_discount": "int64", "total_discount": "int64", "net_value": "int64",
+            "total_cost": "int64", "net_profit": "int64", "gross_profit": "int64"})
+        buysell_df["portfolio_id"] = portfoId
+        insert_to_database(dataframe=buysell_df, database_table="[nooredenadb].[sigma].[buysell]")
 
 
 ##############################
 ##### Updating ProfitLoss Data
-portfoId = 1
-query_last_profitloss = f"select max(date) date FROM [nooredenadb].[sigma].[profitloss] WHERE portfolio_id={portfoId}"
-last_profitloss = pd.read_sql(query_last_profitloss, powerbi_database)["date"].iloc[0]
-start_profitloss = (jdatetime.datetime.strptime(last_profitloss, format="%Y/%m/%d") + one_day).strftime("%Y/%m/%d")
+for pid in [1, 25]:
+    portfoId = pid
+    last_profitloss = pd.read_sql(f"select max(date) date FROM [nooredenadb].[sigma].[profitloss] "
+                                  f"WHERE portfolio_id={portfoId}", powerbi_database)["date"].iloc[0]
+    if last_profitloss:
+        start_profitloss = (jdatetime.datetime.strptime(last_profitloss, format="%Y/%m/%d") + one_day).strftime("%Y/%m/%d")
+    else:
+        start_profitloss = "1404/01/01"
 
-profitloss_data = agent.get_profitloss(start_date=start_profitloss, end_date=today_, portfolio_id=portfoId)
-if len(profitloss_data) > 0:
-
-    profitloss_df = pd.DataFrame(profitloss_data)
-    profitloss_df = profitloss_df[list(cols_mapper_profitloss.keys())]
-    profitloss_df.rename(mapper=cols_mapper_profitloss, axis=1, inplace=True)
-    profitloss_df["portfolio_id"] = portfoId
-
-    insert_to_database(dataframe=profitloss_df, database_table="[nooredenadb].[sigma].[profitloss]")
+    profitloss_data = agent.get_profitloss(start_date=start_profitloss, end_date=today_, portfolio_id=portfoId)
+    if len(profitloss_data) > 0:
+        profitloss_df = pd.DataFrame(profitloss_data)
+        profitloss_df = profitloss_df[list(cols_mapper_profitloss.keys())]
+        profitloss_df.rename(mapper=cols_mapper_profitloss, axis=1, inplace=True)
+        profitloss_df["portfolio_id"] = portfoId
+        insert_to_database(dataframe=profitloss_df, database_table="[nooredenadb].[sigma].[profitloss]")
 
 
 ##############################
 ##### Updating Portfolio Data
-portfoId = 1
-query_last_portfolio = f"SELECT MAX(date) date FROM [nooredenadb].[sigma].[portfolio] WHERE portfolio_id={portfoId}"
-last_portfolio = pd.read_sql(query_last_portfolio, powerbi_database)["date"].iloc[0]
-start_portfolio = (jdatetime.datetime.strptime(last_portfolio, format="%Y/%m/%d") + one_day).strftime("%Y/%m/%d")
-query_end_portfolio = f"SELECT MAX(date) date FROM [nooredenadb].[sigma].[buysell] WHERE portfolio_id={portfoId}"
-end_portfolio = pd.read_sql(query_end_portfolio, powerbi_database)["date"].iloc[0]
+for pid in [1, 25]:
+    portfoId = pid
+    last_portfolio = pd.read_sql(f"SELECT MAX(date) date FROM [nooredenadb].[sigma].[portfolio] "
+                                 f"WHERE portfolio_id={portfoId}", powerbi_database)["date"].iloc[0]
+    if last_portfolio:
+        start_portfolio = (jdatetime.datetime.strptime(last_portfolio, format="%Y/%m/%d") + one_day).strftime("%Y/%m/%d")
+    else:
+        start_portfolio = "1404/01/01"
+    end_portfolio = pd.read_sql(f"SELECT MAX(date) date FROM [nooredenadb].[sigma].[buysell] "
+                                f"WHERE portfolio_id={portfoId}", powerbi_database)["date"].iloc[0]
 
-if end_portfolio >= start_portfolio:
-    portfolio = pd.DataFrame()
-    date_ = start_portfolio
-    while date_ <= end_portfolio:
-        print(f"getting portfolio data for ({date_})")
-        portfolio_data = agent.get_portfolio(date=date_, portfolio_id=portfoId)
-        portfolio_df = pd.DataFrame(portfolio_data)
-        portfolio_df["date"] = date_
-        portfolio = pd.concat([portfolio, portfolio_df], axis=0, ignore_index=True)
-        date_ = (jdatetime.datetime.strptime(date_, format="%Y/%m/%d") + one_day).strftime(format="%Y/%m/%d")
-
-    portfolio = portfolio[list(cols_mapper_portfolio.keys())]
-    portfolio.rename(mapper=cols_mapper_portfolio, axis=1, inplace=True)
-    portfolio["asset"] = portfolio["asset"].str.split("||", regex=False, expand=True)[0]
-    portfolio["portfolio_id"] = portfoId
-    insert_to_database(dataframe=portfolio, database_table="[nooredenadb].[sigma].[portfolio]")
+    if end_portfolio >= start_portfolio:
+        portfolio = pd.DataFrame()
+        date_ = start_portfolio
+        while date_ <= end_portfolio:
+            print(f"getting portfolio data for ({portfoId=} - {date_=})")
+            portfolio_data = agent.get_portfolio(date=date_, portfolio_id=portfoId)
+            if len(portfolio_data) > 0:
+                portfolio_df = pd.DataFrame(portfolio_data)
+                portfolio_df["date"] = date_
+                portfolio = pd.concat([portfolio, portfolio_df], axis=0, ignore_index=True)
+            date_ = (jdatetime.datetime.strptime(date_, format="%Y/%m/%d") + one_day).strftime(format="%Y/%m/%d")
+        portfolio = portfolio[list(cols_mapper_portfolio.keys())]
+        portfolio.rename(mapper=cols_mapper_portfolio, axis=1, inplace=True)
+        portfolio["asset"] = portfolio["asset"].str.split("||", regex=False, expand=True)[0]
+        portfolio["portfolio_id"] = portfoId
+        insert_to_database(dataframe=portfolio, database_table="[nooredenadb].[sigma].[portfolio]")
 
 
 ##############################
 ##### Updating Dividend Data
-portfoId = 1
-query_lst_dividend = f"SELECT MAX(meeting_date) date FROM [nooredenadb].[sigma].[dividend] WHERE portfolio_id={portfoId}"
-lst_dividend = pd.read_sql(query_lst_dividend, powerbi_database)["date"].iloc[0]
-query_end_dividend = f"SELECT MAX(date) date FROM [nooredenadb].[sigma].[portfolio] WHERE portfolio_id={portfoId}"
-end_dividend = pd.read_sql(query_end_dividend, powerbi_database)["date"].iloc[0]
+for pid in [1, 25]:
+    portfoId = pid
+    lst_dividend = pd.read_sql(f"SELECT MAX(meeting_date) date FROM [nooredenadb].[sigma].[dividend] "
+                               f"WHERE portfolio_id={portfoId}", powerbi_database)["date"].iloc[0]
+    if not lst_dividend:
+        lst_dividend = "1404/01/01"
+    end_dividend = pd.read_sql(f"SELECT MAX(date) date FROM [nooredenadb].[sigma].[portfolio] "
+                               f"WHERE portfolio_id={portfoId}", powerbi_database)["date"].iloc[0]
 
-dividend_data = agent.get_dividend(start_date=lst_dividend, end_date=end_dividend, portfolio_id=portfoId)
-dividend_df = pd.DataFrame(dividend_data)
-dividend_df = dividend_df[list(cols_mapper_dividend.keys())]
-dividend_df.rename(mapper=cols_mapper_dividend, axis=1, inplace=True)
-dividend_df["portfolio_id"] = portfoId
-dividend_df = dividend_df[dividend_df["value"] > 0]
-dividend_df = dividend_df[dividend_df["meeting_date"] >= lst_dividend].reset_index(drop=True, inplace=False)
+    dividend_data = agent.get_dividend(start_date=lst_dividend, end_date=end_dividend, portfolio_id=portfoId)
+    dividend_df = pd.DataFrame(dividend_data)
+    dividend_df = dividend_df[list(cols_mapper_dividend.keys())]
+    dividend_df.rename(mapper=cols_mapper_dividend, axis=1, inplace=True)
+    dividend_df["portfolio_id"] = portfoId
+    dividend_df = dividend_df[dividend_df["value"] > 0]
+    dividend_df = dividend_df[dividend_df["meeting_date"] >= lst_dividend].reset_index(drop=True, inplace=False)
 
-if len(dividend_df) > 0:
-    crsr = powerbi_database.cursor()
-    crsr.execute(f"DELETE FROM [nooredenadb].[sigma].[dividend] WHERE meeting_date >= '{lst_dividend}'")
-    crsr.close()
-    insert_to_database(dataframe=dividend_df, database_table="[nooredenadb].[sigma].[dividend]")
+    if len(dividend_df) > 0:
+        crsr = powerbi_database.cursor()
+        crsr.execute(f"DELETE FROM [nooredenadb].[sigma].[dividend] "
+                     f"WHERE portfolio_id={portfoId} AND meeting_date >= '{lst_dividend}'")
+        crsr.close()
+        insert_to_database(dataframe=dividend_df, database_table="[nooredenadb].[sigma].[dividend]")
 
