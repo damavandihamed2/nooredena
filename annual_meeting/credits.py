d8cb6c6490a89810d5c81bc197862608b66beba0
Hamed Damavandi <damavandi.hamed6@yahoo.com>
2026-02-19 18:22:32 +0330

    Update credits.py

--- annual_meeting/credits.py
+++ annual_meeting/credits.py
@@ -1,40 +1,25 @@
 import warnings
-import pandas as pd
 
-from utils.database import make_connection
-from annual_meeting.utils.data import get_credits
+from annual_meeting.utils.data import get_credits, get_portfolio_data_daily
 
-start_day = "1403/10/30"
+
 warnings.filterwarnings("ignore")
-db_conn = make_connection()
 
-##################################################
+s_date = "1403/10/30"
+e_date = "1404/10/30"
 
-credits_df = get_credits(start_date="1403/10/30", end_date="1404/10/30")
+credits_df = get_credits(start_date=s_date, end_date=e_date)
+portfolio_df = get_portfolio_data_daily(start_date=s_date, end_date=e_date)
 
+portfolio_df = portfolio_df.merge(credits_df, how="left")
 
-sigma_portfolio = pd.read_sql("SELECT date, sum(total_cost) as cost, sum(gross_value_final_price) as value "
-                              f"FROM [nooredenadb].[sigma].[portfolio] where date>='{start_day}' and "
-                              "type in ('صندوق', 'گواهی سپرده کالایی', 'حق تقدم', 'سهام') "
-                              "group by date order by date", db_conn)
-sigma_dividend = pd.read_sql("SELECT meeting_date as date, sum(value) as dividend FROM "
-                             f"[nooredenadb].[sigma].[dividend] where meeting_date >= '{start_day}' and "
-                             "value > 0 group by meeting_date order by meeting_date", db_conn)
-sigma_profit = pd.read_sql("SELECT date, sum(net_profit) as profit FROM [nooredenadb].[sigma].[buysell] where "
-                           f"date >= '{start_day}' and action='فروش' and type!='اختیار معامله' group by date "
-                           "order by date", db_conn)
-
-dataframe = sigma_portfolio.merge(sigma_dividend, on="date", how="outer")
-dataframe = dataframe.merge(sigma_profit, on="date", how="outer")
-dataframe = dataframe.merge(credits_df, on="date", how="outer")
-
-dataframe["remaining"].ffill(inplace=True)
-dataframe["remaining_yesterday"] = dataframe["remaining"].shift(1)
-dataframe["value_yesterday"] = dataframe["value"].shift(1)
-dataframe["cost_yesterday"] = dataframe["cost"].shift(1)
-dataframe.fillna({"dividend": 0, "profit": 0}, inplace=True)
-dataframe["portfo_return"] = ((dataframe["value"] + dataframe["dividend"] + dataframe["profit"] -
-                               dataframe["cost"] - (dataframe["value_yesterday"] - dataframe["cost_yesterday"])
-                               ) / dataframe["value_yesterday"]) + 1
-dataframe["credit_profit"]  = (dataframe["portfo_return"] - 1) * dataframe["remaining_yesterday"]
-dataframe.to_excel("c:/users/h.damavandi/desktop/dataframe.xlsx", index=False)
+portfolio_df["remaining"].ffill(inplace=True)
+portfolio_df["remaining_yesterday"] = portfolio_df["remaining"].shift(1)
+portfolio_df["value_yesterday"] = portfolio_df["value"].shift(1)
+portfolio_df["cost_yesterday"] = portfolio_df["cost"].shift(1)
+portfolio_df.fillna({"dividend": 0, "profit": 0}, inplace=True)
+portfolio_df["portfo_return"] = (portfolio_df["value"] + portfolio_df["dividend"] + portfolio_df["profit"] -
+                                 portfolio_df["cost"] - (portfolio_df["value_yesterday"] -
+                                                         portfolio_df["cost_yesterday"])) / portfolio_df["value_yesterday"]
+portfolio_df["credit_profit"] = portfolio_df["portfo_return"] * portfolio_df["remaining_yesterday"]
+portfolio_df.to_excel(f"./annual_meeting/portfolio_credits.xlsx", index=False)
