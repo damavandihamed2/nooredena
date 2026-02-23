676830b86f1be5547b6b52b224a9638adad4a07f
Hamed Damavandi <damavandi.hamed6@yahoo.com>
2026-02-23 18:07:04 +0330

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,23 +1,29 @@
+import os
 import time
 import pyodbc
 import numpy as np
 from tqdm import tqdm
+from pandas import DataFrame
+from dotenv import load_dotenv
 
 
-db_server_ip = "172.20.30.34"
-db_name = "nooredenadb"
+load_dotenv(dotenv_path=".env", override=False)
+
+db_server_ip = os.getenv("DB_SERVER")
+db_server_name = os.getenv("DB_NAME")
+db_username = os.getenv("DB_USERNAME")
+db_password = os.getenv("DB_PASSWORD")
+
 authenticators = {
-    "admin": {"username": "sa", "password": "l0XZD6y$B86h"},
     "public": {"username": "userpublic", "password": "Nooredena@public.123"}
 }
 
-
 def make_connection(
-        username: str = authenticators["admin"]["username"],
-        password: str = authenticators["admin"]["password"],
+        username: str = db_username,
+        password: str = db_password,
         auto_commit: bool = True, timeout: int = 10, retries: int = 10, delay: int = 3
-):
-    conn_str = (f"DRIVER={{ODBC Driver 18 for SQL Server}}; SERVER={db_server_ip}; DATABASE={db_name}; "
+) -> pyodbc.Connection | None:
+    conn_str = (f"DRIVER={{ODBC Driver 18 for SQL Server}}; SERVER={db_server_ip}; DATABASE={db_server_name}; "
                 f"UID={username}; PWD={password}; Encrypt=No")
     for attempt in range(1, retries + 1):
         try:
@@ -29,7 +35,7 @@ def make_connection(
             time.sleep(delay)
 
 
-def insert_to_database(dataframe, database_table, loading=True, batch_size: int = 1000):
+def insert_to_database(dataframe: DataFrame, database_table: str, loading: bool = True, batch_size: int = 1000) -> None:
     db_conn = make_connection()
     cursor = db_conn.cursor()
     cursor.fast_executemany = True
