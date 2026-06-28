40ea5ff3a95a022087e97202809e980130eff88a
Hamed Damavandi <damavandi.hamed6@yahoo.com>
2026-06-28 17:10:07 +0330

    Update get_1404_data.py

--- mci/racing_bar_chart/get_1404_data.py
+++ mci/racing_bar_chart/get_1404_data.py
@@ -1,8 +1,10 @@
 import warnings
-import requests
 import pandas as pd
 from tqdm import tqdm
+
 from utils.database import make_connection
+from mci.racing_bar_chart.utils.get_data import get_total_share, get_final_price
+
 
 
 warnings.filterwarnings("ignore")
@@ -21,50 +23,6 @@ symbols = symbols[symbols["final_last_date"] >= 20260101]
 symbols = symbols[symbols["last_date"] >= 20260101]
 symbols["total_share"] = None
 symbols["final_price"] = None
-
-def get_total_share(symbol_id: str, date: int) -> int:
-    headers = {
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'en-US,en;q=0.9,fa;q=0.8',
-        'cache-control': 'no-cache',
-        'origin': 'https://www.tsetmc.com',
-        'pragma': 'no-cache',
-        'priority': 'u=1, i',
-        'referer': 'https://www.tsetmc.com/',
-        'sec-ch-ua': '"Google Chrome";v="149", "Chromium";v="149", "Not)A;Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36',
-    }
-    response = requests.get(f'https://cdn.tsetmc.com/api/Instrument/GetInstrumentHistory/{symbol_id}/{date}', headers=headers)
-    total_share = response.json()["instrumentHistory"].get("zTitad", None)
-    return total_share
-
-
-def get_final_price(symbol_id: str, date: int) -> int:
-    headers = {
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'en-US,en;q=0.9,fa;q=0.8',
-        'cache-control': 'no-cache',
-        'origin': 'https://www.tsetmc.com',
-        'pragma': 'no-cache',
-        'priority': 'u=1, i',
-        'referer': 'https://www.tsetmc.com/',
-        'sec-ch-ua': '"Google Chrome";v="149", "Chromium";v="149", "Not)A;Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36',
-    }
-    response_ = requests.get(f'https://cdn.tsetmc.com/api/ClosingPrice/GetClosingPriceDaily/{symbol_id}/{date}', headers=headers)
-    final_price = response_.json()["closingPriceDaily"].get("pClosing", None)
-    return final_price
-
 
 for i in tqdm(range(len(symbols))):
     s_id = symbols["symbol_id"].iloc[i]
